938ce18df9116cbdb027c49a83e259395c9e6a43
ryuya-obuchi <ryuya-obuchi@cybozu.co.jp>
2024-07-02 14:54:25 +0900

    JSAPI2

---
+++ task7/task7.js
@@ -0,0 +1,29 @@
+(() => {
+  'use strict';
+  //イベント：フィールドの値が変更されたとき, app.record.edit.change.フィールドコード
+  //入力：date, product, number
+  //加工：YYYYMMDD-00-++に整形
+  //出力：文字列に整形したものを出力
+  const proName = {
+    kintone: 'KN',
+    Garoon: 'GR',
+    "サイボウズ Office": 'OF',
+    Mailwise: 'MW'
+  };
+  var events = [
+    'app.record.create.show',
+    'app.record.edit.show',
+    'app.record.edit.change.date',
+    'app.record.edit.change.products',
+    'app.record.edit.change.number',
+    'app.record.create.change.date',
+    'app.record.create.change.products',
+    'app.record.create.change.number'
+  ];
+  kintone.events.on(events, (event) => {
+    const newDate = dateFns.format(event.record.date.value, 'yyyyMMdd');
+    event.record.output.value = `${newDate}-${proName[event.record.products.value]}-${event.record.number.value}`;
+    event.record['output'].disabled = true;
+    return event;
+  });
+})();
